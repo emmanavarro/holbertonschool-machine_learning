09acaddf40644fc95fd14685c7b9c314a8d6e555
Emma Navarro <elnavarro55@gmail.com>
2020-12-01 20:19:13 -0500

    Multiplying two matrices with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Script to perform matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the multiplication between two matrices"""
+    return np.matmul(mat1, mat2)
