76594f5734b98b3dbda0304c21e254a90929609e
Emma Navarro <elnavarro55@gmail.com>
2020-12-09 12:17:37 -0500

    Function to calculate a derivative

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Function that calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """
+    Calculates the derivative of a polynomial
+    """
+    if type(poly) != list or len(poly) == 0:
+        return None
+
+    deriv_pol = []
+    for x in range(len(poly)):
+        deriv_pol.append(poly[x] * x)
+    return deriv_pol[1:]
+
+    if deriv_pol == 0:
+        return [0]
