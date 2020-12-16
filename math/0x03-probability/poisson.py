f44fd2061b0a561cdbdf8d593d2fecb2a909991c
Emma Navarro <elnavarro55@gmail.com>
2020-12-15 22:21:23 -0500

    isinstance method added

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -7,13 +7,13 @@ class Poisson:
     def __init__(self, data=None, lambtha=1.):
         """ Sets the instance attribute lambtha """
         if data is not None:
-            if not list:
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
         else:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
 
