2caab65053880856fba25be2769b1b8e7326625b
Emma Navarro <elnavarro55@gmail.com>
2020-12-15 22:47:16 -0500

    Represent normal distribution

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Create a class Normal that represents a normal distribution """
+
+
+class Normal:
+    """ Normal class """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ Class constructor """
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            list_data = [(x - self.mean) ** 2 for x in data]
+            self.stddev = (sum(list_data) / len(data)) ** 0.5
+        else:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
