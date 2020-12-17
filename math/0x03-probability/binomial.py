286e2071287f6866bf57111a3d369884dcf7cd91
Emma Navarro <elnavarro55@gmail.com>
2020-12-16 21:22:49 -0500

    pmf method was added

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -26,3 +26,25 @@ class Binomial:
             if (p <= 0) or (p >= 1):
                 raise ValueError("p must be greater than 0 and less than 1")
             self.p = float(p)
+
+    @staticmethod
+    def factorial(n):
+        """ Calculates the factorial of a given number """
+        factorial_num = 1
+        for m in range(1, n + 1):
+            factorial_num *= m
+        return factorial_num
+
+    def pmf(self, k):
+        """ Calculates the value of the PMF
+            for a given number of “successes” """
+        k = int(k)
+        if (k < 0) or (k > self.n):
+            return 0
+
+        n_factorial = self.factorial(self.n)
+        k_factorial = self.factorial(k)
+        nk_factorial = self.factorial(self.n - k)
+        binomial_coefficient = n_factorial / (nk_factorial * k_factorial)
+        q = 1 - self.p
+        return binomial_coefficient * ((self.p ** k) * (q ** (self.n - k)))
