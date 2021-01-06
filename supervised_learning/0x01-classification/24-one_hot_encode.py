4295e4bf522accc1424827b3cdf2f447638cf5eb
emmanavarro <elnavarro55@gmail.com>
2021-01-05 23:51:28 -0500

    Task 24 added

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+One-Hot Encode function
+"""
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numeric label vector into a one-hot matrix
+    Args:
+        - Y: is a numpy.ndarray with shape (m,) containing numeric class
+             labels.
+            - m: is the number of examples.
+        - classes: is the maximum number of classes found in Y.
+    Return:
+        A one-hot encoding of Y with shape (classes, m), or None on failure.
+    """
+    if not isinstance(Y, np.ndarray):
+        return None
+    if not isinstance(classes, int):
+        return None
+    one_hot = np.eye(classes)[Y].T
+    return one_hot
