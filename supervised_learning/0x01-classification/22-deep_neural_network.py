68c48f7fb8fb8e72e9feee744ca1fd9a8b0e2669
emmanavarro <elnavarro55@gmail.com>
2021-01-05 21:47:55 -0500

    Task 22 added

--- supervised_learning/0x01-classification/22-deep_neural_network.py
+++ supervised_learning/0x01-classification/22-deep_neural_network.py
@@ -178,12 +178,12 @@ class DeepNeuralNetwork:
         """
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
-        if iterations <= 0:
+        if iterations < 0:
             raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
-        if alpha <= 0:
-            raise ValueError("alpha must be a positive")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
 
         for i in range(iterations):
             self.forward_prop(X)
