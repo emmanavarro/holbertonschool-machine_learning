61b18652e1ce69cfe2d3e771c2a648a8a47d3d9c
emmanavarro <elnavarro55@gmail.com>
2021-01-08 00:46:15 -0500

    Training model

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 """
-Function to build, train, and save a neural network classifier
+Builds, trains, and saves a neural network classifier
 """
 
+
 import tensorflow as tf
-
-
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
-          iterations, save_path="/tmp/model.ckpt"):
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
     """
-    Builds, trains, and saves a neural network classifier.
+    Builds, trains, and saves a neural network classifier
     Args:
         - X_train: is a numpy.ndarray containing the training input data.
         - Y_train: is a numpy.ndarray containing the training labels.
@@ -32,9 +31,10 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
     Return:
         The path where the model was saved.
     """
+    step = 100
+
     # Placeholders x, y
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    # Add to graph collection
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
 
@@ -53,30 +53,29 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
     tf.add_to_collection('train_op', train_op)
 
     # Setting global initializer
-    init = tf.global_variables_initializer()
+    init_op = tf.global_variables_initializer()
 
     # Setting the operation to save and restore all variables
     saver = tf.train.Saver()
 
-    # Launching the graph and training, saving the model every 100 iterations
-    step = 100
-
     with tf.Session() as sess:
-        sess.run(init)
+        sess.run(init_op)
 
-        # Using the context manager
         for i in range(iterations + 1):
-            t_cost, t_acc = sess.run([loss, accuracy],
-                                     feed_dict={x: X_train, y: Y_train})
-            v_cost, v_acc = sess.run([loss, accuracy],
-                                     feed_dict={x: X_valid, y: Y_valid})
+            t_cost, t_accuracy = sess.run(
+                [loss, accuracy],
+                feed_dict={x: X_train, y: Y_train})
+
+            v_cost, v_accuracy = sess.run(
+                [loss, accuracy],
+                feed_dict={x: X_valid, y: Y_valid})
 
             if i % step == 0 or i == iterations:
-                print("After {} iterations".format(i))
+                print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(t_cost))
-                print("\tTraining Accuracy: {}".format(t_acc))
+                print("\tTraining Accuracy: {}".format(t_accuracy))
                 print("\tValidation Cost: {}".format(v_cost))
-                print("\tValidation Accuracy: {}".format(v_acc))
+                print("\tValidation Accuracy: {}".format(v_accuracy))
 
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
