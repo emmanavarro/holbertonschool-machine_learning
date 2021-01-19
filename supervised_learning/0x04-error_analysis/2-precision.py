e0dfbd5916592e07ac000f6d5e5f4cef3784a46a
emmanavarro <elnavarro55@gmail.com>
2021-01-18 20:53:56 -0500

    Precision function

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Calculates the precision in a confusion matrix
+"""
+
+import numpy as np
+
+
+def precision(confusion):
+    """
+    calculates the precision for each class in a confusion matrix
+    Args:
+    - confusion: is a confusion numpy.ndarray of shape (classes, classes) where
+      row indices represent the correct labels and column indices represent the
+      predicted labels
+        * classes: is the number of classes
+    Return:
+    A numpy.ndarray of shape (classes,) containing the precision of each class
+    """
+    true_positive = np.diagonal(confusion)
+    false_positive = np.sum(confusion, axis=0)
+    precision = true_positive / (true_positive + false_positive)
+
+    return precision
